07b63f2864985a3e24f2d93a18cece1387fad875
kittyfanclub <35534663+kittyfanclub@users.noreply.github.com>
2018-01-18 10:58:02 -0600

    Add files via upload

--- views/js/includes/footer.js
+++ views/js/includes/footer.js
@@ -1,6 +1,4 @@
 document.write("<div class='footer'>");
 document.write("Crytokittes Fan Club is completely free.  If you would like to support development of new features, please consider a donation.");
-document.write("<li>Etherium donation address: 0xe8dc1acb0c836d42d99a1f613a11abcdf9b060a2");
-document.write("<li> Litecoin donation address: LWdSmTkEfNnb5xGD1AmNkxBmXUGcxoqKf8");
-document.write("<li> Bitcoin donation address: 3LErNcN7WMUbCgeSxgppBS3b1GZAMU8zj3");
+document.write("<li>Etherium donation address: 0x464eFb9448bDd6030564761FA8E7986FEc1bA9Cc");
 document.write("</div>");
