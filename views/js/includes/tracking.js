7ffd86103340d4729206b6e68497fbe09b62c691
kittyfanclub <35534663+kittyfanclub@users.noreply.github.com>
2018-01-22 12:38:47 -0600

    Add files via upload

---
+++ views/js/includes/tracking.js
@@ -0,0 +1,7 @@
+document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-112877476-1\"></script>");
+document.write("<script>");
+document.write("window.dataLayer = window.dataLayer || [];");
+document.write("function gtag(){dataLayer.push(arguments);}");
+document.write("gtag('js', new Date());");
+document.write("gtag('config', 'UA-112877476-1');");
+document.write("</script>");
