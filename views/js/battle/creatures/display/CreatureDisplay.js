db2dc7de461406dab6ff809c9f821647e87e1a21
kittyfanclub <35534663+kittyfanclub@users.noreply.github.com>
2018-01-24 15:13:50 -0600

    Add files via upload

--- views/js/battle/creatures/display/CreatureDisplay.js
+++ views/js/battle/creatures/display/CreatureDisplay.js
@@ -1,1 +1,10 @@
-
+class CreatureDisplay {
+  constructor(_left, _top, _width, _height, _imgAddress, _img) {
+    this.left = _left;
+    this.top = _top;
+    this.width = _width;
+    this.height = _height;
+    this.imgAddress = _imgAddress;
+    this.img = _img;
+  }
+}
