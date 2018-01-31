743a45d20b8f7490385c6b716edb572689a4803a
kittyfanclub <35534663+kittyfanclub@users.noreply.github.com>
2018-01-31 14:34:05 -0600

    Add files via upload

--- views/js/includes/footer.js
+++ views/js/includes/footer.js
@@ -1,3 +1,4 @@
+// This code is licensed under the GNU General Public License found at: kittyfanclub.github.io/license.txt
 document.write("<div class='footer'>");
 document.write("Crytokittes Fan Club is completely free.  If you would like to support development of new features, please consider a donation.");
 document.write("<li>Etherium donation address: 0x464eFb9448bDd6030564761FA8E7986FEc1bA9Cc");
--- views/js/includes/tracking.js
+++ views/js/includes/tracking.js
@@ -1,3 +1,4 @@
+// This code is licensed under the GNU General Public License found at: kittyfanclub.github.io/license.txt
 document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-112877476-1\"></script>");
 document.write("<script>");
 document.write("window.dataLayer = window.dataLayer || [];");
