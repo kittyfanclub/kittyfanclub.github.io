c1263a234e95c5dfa27c8c613599df1831aa0b98
kittyfanclub <35534663+kittyfanclub@users.noreply.github.com>
2018-01-31 14:26:12 -0600

    Add files via upload

--- views/js/battle/battleIncludes.js
+++ views/js/battle/battleIncludes.js
@@ -1,4 +1,6 @@
+// This code is licensed under the GNU General Public License found at: kittyfanclub.github.io/license.txt
 var rootDir = "js/battle/"
+document.write("<script src=\"" + rootDir + "creatures/display/damage/WoundImage.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/attributes/CreatureAttributes.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/display/CreatureDisplay.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/display/DisplayManager.js" + "\"></script>");
@@ -6,9 +8,14 @@ document.write("<script src=\"" + rootDir + "creatures/moves/BattleMove.js" + "\
 document.write("<script src=\"" + rootDir + "creatures/moves/AttackMove.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/moves/CreatureMoves.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/moves/DefenseMove.js" + "\"></script>");
+document.write("<script src=\"" + rootDir + "creatures/moves/HealMove.js" + "\"></script>");
+document.write("<script src=\"" + rootDir + "creatures/moves/LingeringAttackMove.js" + "\"></script>");
+
 document.write("<script src=\"" + rootDir + "creatures/BattleCreature.js" + "\"></script>");
+document.write("<script src=\"" + rootDir + "creatures/dataloader/kitties/KittyWounds.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/dataloader/kitties/KittyAttacks.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/dataloader/kitties/KittyDataLoader.js" + "\"></script>");
+document.write("<script src=\"" + rootDir + "creatures/display/ImageUtils.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/display/animation/AttackAnimations.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "creatures/display/animation/EndGameAnimation.js" + "\"></script>");
 document.write("<script src=\"" + rootDir + "damage/BattleDamage.js" + "\"></script>");
